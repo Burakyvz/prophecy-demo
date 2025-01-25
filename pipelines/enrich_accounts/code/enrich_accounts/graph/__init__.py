5a2a2c7d70f6eb6c73f9d720b77aceb382062a23
burak.yvz01@gmail.com <burak.yvz01@gmail.com>
2025-01-25 18:03:59 +0000

    Add enrich_accounts pipeline, enriched_accounts dataset, and daily_accounts job

---
+++ pipelines/enrich_accounts/code/enrich_accounts/graph/__init__.py
@@ -0,0 +1,6 @@
+from .multi_join import multi_join
+from .byAccount import byAccount
+from .SelectFields import SelectFields
+from .salesforce_Opportunity import salesforce_Opportunity
+from .salesforce_Account import salesforce_Account
+from .enriched_accounts import enriched_accounts
